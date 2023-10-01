984ce19f3373bf05503f68df9ca36e17eea3b514
Jonathan Moratelli <jonathan.jmc@gmail.com>
2023-10-01 14:27:11 -0300

    tests

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # sql2spreadsheet
 An easy way to turn an SQL into a spreadsheet
+
+
+Running the Tests:
+    - Execute all tests
+        - pytest
+    - Code Coverage
+        - coverage run -m pytest
+        - coverage report
+        - coverage html
+
+
+
+References:
+    - https://flask.palletsprojects.com/en/3.0.x/tutorial/
---
+++ tests/test_auth.py
@@ -0,0 +1,65 @@
+import pytest
+from flask import g, session
+from sql2spreadsheet.db import get_db
+
+
+def test_register(client, app):
+    assert client.get("/auth/register").status_code == 200
+    response = client.post("/auth/register", data={"email": "a", "password": "a"})
+    assert response.headers["Location"] == "/auth/login"
+
+    with app.app_context():
+        assert (
+            get_db()
+            .execute(
+                "SELECT * FROM user WHERE email = 'a'",
+            )
+            .fetchone()
+            is not None
+        )
+
+
+@pytest.mark.parametrize(
+    ("email", "password", "message"),
+    (
+        ("", "", b"email is required."),
+        ("a", "", b"Password is required."),
+        ("test@test.com", "test", b"already registered"),
+    ),
+)
+def test_register_validate_input(client, email, password, message):
+    response = client.post(
+        "/auth/register", data={"email": email, "password": password}
+    )
+    assert message in response.data
+
+
+def test_login(client, auth):
+    assert client.get("/auth/login").status_code == 200
+    response = auth.login()
+    assert response.headers["Location"] == "/"
+
+    with client:
+        client.get("/")
+        assert session["user_id"] == 1
+        assert g.user["email"] == "test@test.com"
+
+
+@pytest.mark.parametrize(
+    ("email", "password", "message"),
+    (
+        ("a", "test", b"Incorrect email."),
+        ("test@test.com", "a", b"Incorrect password."),
+    ),
+)
+def test_login_validate_input(auth, email, password, message):
+    response = auth.login(email, password)
+    assert message in response.data
+
+
+def test_logout(client, auth):
+    auth.login()
+
+    with client:
+        auth.logout()
+        assert "user_id" not in session
